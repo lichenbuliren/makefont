0fe8f7daf62a84bc30abcb1fbc02b377ae54fc2e
lichenbuliren <617106268@qq.com>
2017-05-02 20:13:56 +0800

    优化代码

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
 #!/usr/bin/env node
 
-// commaner 模块
-var program = require('commander');
-var makefont = require('./lib/makefont');
-var fs = require('fs');
-
-var path = require('path');
-var sourcePath, destPath, fontPath;
-
-program.version(require('./package').version)
-  .usage('[options]')
-  .option('-f, --font <font>', '字体源文件或目录', makefont.validateFilePath)
-  .option('-t, --target <target>', '字体生成目录，默认当前目录', '.')
-  .option('-s, --source <dirs>', '需要被抽取的文字来源文件或目录, 多个目录以逗号隔开，支持正则匹配', makefont.strToArray)
-  .parse(process.argv);
-
-if (program.font) {
-  fontPath = program.font;
-} else {
-  console.error('invalid font dir path');
-  process.exit(1);
-}
-
-if (program.source) {
-  sourcePath = program.source;
-} else {
-  console.error('invalid source dir path');
-  process.exit(1);
-}
-
-if (program.target) {
-  destPath = program.target;
-} else {
-  destPath = path.resolve(process.cwd(), '.');
-}
-
-console.log('start fontmin......');
-makefont.fontmin(fontPath, sourcePath, destPath, function () {
-  console.log('恭喜！，字体文件生成完毕。');
-});
+var makefont = require('./lib');
+makefont.progress(makefont.init(process.argv));
